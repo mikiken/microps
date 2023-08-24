62fb376a97a16803cb4129d2b3420ca0e7c1b83e
Kentaro Miki <mikiken.dev@gmail.com>
2023-08-24 18:38:39 +0900

    Add template

---
+++ ip.c
@@ -0,0 +1,10 @@
+#include <stddef.h>
+#include <stdint.h>
+
+#include "ip.h"
+#include "net.h"
+#include "util.h"
+
+static void ip_input(const uint8_t *data, size_t len, struct net_device *dev) {}
+
+int ip_init(void) {}
