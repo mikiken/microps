cc6fd779d6bde55aaab41d42d84f2a65b1ede3e5
Kentaro Miki <mikiken.dev@gmail.com>
2023-08-24 16:45:33 +0900

    Add template

--- driver/dummy.c
+++ driver/dummy.c
@@ -2,10 +2,14 @@
 #include <stdint.h>
 #include <stdio.h>
 
+#include "platform.h"
+
 #include "net.h"
 #include "util.h"
 
 #define DUMMY_MTU UINT16_MAX /* maximum size of IP datagram */
+
+#define DUMMY_IRQ INTR_IRQ_BASE
 
 static int dummy_transmit(struct net_device *dev, uint16_t type, const uint8_t *data, size_t len, const void *dst) {
   debugf("dev=%s, type=0x%04x, len=%zu", dev->name, type, len);
@@ -13,6 +17,8 @@ static int dummy_transmit(struct net_device *dev, uint16_t type, const uint8_t *
   /* drop data */
   return 0;
 }
+
+static int dummy_isr(unsigned int irq, void *id) {}
 
 static struct net_device_ops dummy_ops = {
     .transmit = dummy_transmit,
