e00c220916dc70f74e852b9888934ef6b194c781
Kentaro Miki <mikiken.dev@gmail.com>
2023-08-25 17:14:22 +0900

    Add template

---
+++ icmp.c
@@ -0,0 +1,10 @@
+#include <stddef.h>
+#include <stdint.h>
+
+#include "icmp.h"
+#include "ip.h"
+#include "util.h"
+
+void icmp_input(const uint8_t *data, size_t len, ip_addr_t src, ip_addr_t dst, struct ip_iface *iface) {}
+
+int icmp_init(void) {}
